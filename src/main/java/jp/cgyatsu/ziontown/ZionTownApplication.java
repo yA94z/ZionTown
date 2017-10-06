3f11f055e67b19e2c841948987314d418a285e65
yA94z <qqgu8rq9k@theia.ocn.ne.jp>
2017-10-07 06:39:23 +0900

    first commit

---
+++ src/main/java/jp/cgyatsu/ziontown/ZionTownApplication.java
@@ -0,0 +1,12 @@
+package jp.cgyatsu.ziontown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZionTownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZionTownApplication.class, args);
+	}
+}
---
+++ src/test/java/jp/cgyatsu/ziontown/ZionTownApplicationTests.java
@@ -0,0 +1,16 @@
+package jp.cgyatsu.ziontown;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ZionTownApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
